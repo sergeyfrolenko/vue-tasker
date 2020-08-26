4409958cd92e789d9a7358dab0c689a3efd64bfb
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-08-26 14:08:12 +0300

    v0.1 home page, plus materialize

---
+++ vue-tasker/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+</style>
---
+++ vue-tasker/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>hello from Hove.vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
